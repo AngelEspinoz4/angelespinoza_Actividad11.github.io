fe653e1f253754cb334ea8fdd6eb01e18ee8fade
Angel Espinoza <135487614+AngelEspinoz4@users.noreply.github.com>
2023-12-03 00:09:01 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "https://angelespinoz4.github.io/angelespinoza_Actividad11.github.io/"
+  base: "/angelespinoza_Actividad11.github.io/",
 });
